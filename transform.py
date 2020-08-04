1c9f1ab96984eb090df52884ce23dfe7b5cee4cf
tropicalmentat <gabtorres1011@gmail.com>
2020-08-05 07:03:10 +0800

    Functionized logic of extracting timestamps and the timelapse of traffic update

--- transform.py
+++ transform.py
@@ -1,27 +1,50 @@
 import datetime as dt
+# import great_expectations as ge
+import logging
 
 data_fpath = r'/home/gtorres/Desktop/transform_scrape_traffic_data/data/20200803/trfc_stat_20200803_121502.csv'
+
+def get_minutes(timestamp):
+	return timestamp.split(" ")[0].split(":")[1]
+
+def get_scrape_timestamp(line_status):
+	return cln_ln.split(",")[-1]
+
+def get_northbound_timestamp(line_status):
+	return cln_ln.split(",")[3]
+
+def calc_actual_timestamp(timestamp,timedelta):
+	return 	dt.datetime.strptime(timestamp,"%Y-%m-%d %H:%M")-timedelta
+
+def get_update_timelapse(timestamp):
+	duration = int(timestamp.split(" ")[2].strip('('))
+	unit = timestamp.split(" ")[3]
+	return duration,unit
+
 
 with open(data_fpath, "r") as fobj:
 	for i,ln in enumerate(fobj):
 		cln_ln = ln.strip('\n')
 		if i > 0:
-			scrp_ts = cln_ln.split(",")[-1]
-			nb_ts = cln_ln.split(",")[3]
+			scrp_ts = get_scrape_timestamp(cln_ln)
+			nb_ts = get_northbound_timestamp(cln_ln)
 
-			duration = int(nb_ts.split(" ")[2].strip('('))
-			unit = nb_ts.split(" ")[3]
+			nb_ts_mins = get_minutes(nb_ts)
+
+			duration = get_update_timelapse(nb_ts)[0]
+			unit = get_update_timelapse(nb_ts)[1]
 
 			if unit=='seconds':
-				td=dt.timedelta(seconds=duration)
+				td = dt.timedelta(seconds=duration)
 			elif unit=='hrs':
 				td = dt.timedelta(hours=duration)
 			elif unit=="days":
 				td = dt.timedelta(days=duration)
 
 			try:
-				print(nb_ts,dt.datetime.strptime(scrp_ts,"%Y-%m-%d %H:%M")-td)
+				actual_ts =  calc_actual_timestamp(scrp_ts,td)
 			except Exception as e:
 				print(e)
 
+			# TODO: Need to generalize extraction of timestamps from northbound and southbound data
 			# TODO: replace of derived timestamp with actual time of update
